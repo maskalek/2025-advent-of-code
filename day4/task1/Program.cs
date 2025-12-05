62f7807fc08ac5ec184519ae8e8b2a56b55b114d
Pavel Maskevich <pavel.maskevich@gmail.com>
2025-12-04 20:01:56 -0500

    add day 4

---
+++ day4/task1/Program.cs
@@ -0,0 +1,50 @@
+﻿// var lines = File.ReadLines("day4/task1/test.txt").ToArray();
+var lines = File.ReadLines("day4/task1/puzzle.txt").ToArray();
+var totalForklifts = 0;
+for(var r = 0; r < lines.Length; r++)
+{
+    var liftsOnThisLine = 0;
+    for(var c = 0; c < lines[r].Length; c++)
+    {
+        if(IsValid(r, c))
+        {
+            liftsOnThisLine++;
+        }
+    }
+    totalForklifts += liftsOnThisLine;
+    Console.WriteLine($"Line {lines[r]}: {liftsOnThisLine} accessible forklifts");
+}
+
+Console.WriteLine($"Total accessible forklifts: {totalForklifts}");
+
+bool IsValid(int i, int j)
+{
+    if(lines[i][j] != '@')
+    {
+        return false;
+    }
+    var coordinates = new (int, int)[]
+    {
+        (i-1, j), // up
+        (i+1, j), // down
+        (i, j-1), // left
+        (i, j+1),  // right
+        (i-1, j-1), // up-left
+        (i-1, j+1), // up-right
+        (i+1, j-1), // down-left
+        (i+1, j+1)  // down-right
+    };
+    var count = 0;
+    foreach(var (x, y) in coordinates)
+    {
+        if(x < 0 || y < 0 || x >= lines.Length || y >= lines[0].Length)
+        {
+            continue;
+        }
+        if(lines[x][y] == '@')
+        {
+            count++;
+        }
+    }
+    return count < 4;
+}
---
+++ day4/task2/Program.cs
@@ -0,0 +1,60 @@
+﻿// var lines = File.ReadLines("day4/task2/test.txt").Select(x => x.ToCharArray()).ToArray();
+var lines = File.ReadLines("day4/task2/puzzle.txt").Select(x => x.ToCharArray()).ToArray();
+var totalForklifts = 0;
+while(true)
+{
+    var removed = 0;
+    for(var r = 0; r < lines.Length; r++)
+    {
+        var liftsOnThisLine = 0;
+        for(var c = 0; c < lines[r].Length; c++)
+        {
+            if(IsValid(r, c))
+            {
+                liftsOnThisLine++;
+                removed++;
+                lines[r][c] = '.';
+            }
+        }
+        totalForklifts += liftsOnThisLine;
+        Console.WriteLine($"Line {lines[r]}: {liftsOnThisLine} accessible forklifts");
+    }
+    if(removed == 0)
+    {
+        break;
+    }
+}
+
+Console.WriteLine($"Total accessible forklifts: {totalForklifts}");
+
+bool IsValid(int i, int j)
+{
+    if(lines[i][j] != '@')
+    {
+        return false;
+    }
+    var coordinates = new (int, int)[]
+    {
+        (i-1, j), // up
+        (i+1, j), // down
+        (i, j-1), // left
+        (i, j+1),  // right
+        (i-1, j-1), // up-left
+        (i-1, j+1), // up-right
+        (i+1, j-1), // down-left
+        (i+1, j+1)  // down-right
+    };
+    var count = 0;
+    foreach(var (x, y) in coordinates)
+    {
+        if(x < 0 || y < 0 || x >= lines.Length || y >= lines[0].Length)
+        {
+            continue;
+        }
+        if(lines[x][y] == '@')
+        {
+            count++;
+        }
+    }
+    return count < 4;
+}
